3ee4ba9b576b7457894bcbf02545fb23d27dff1b
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-03-14 13:57:51 -0500

    finished part 1

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -4,4 +4,34 @@ public class Student {
     private long id;
     private String name;
     private ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+        grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double total = 0;
+        for (Integer grade : grades) {
+            total += grade;
+        }
+        return total / grades.size();
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,32 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    @Test
+    public void testConstructor() {
+        Student adam = new Student("Adam", 1);
+        assertEquals("Adam", adam.getName());
+        assertEquals(1, adam.getId());
+        assertEquals(new ArrayList<Integer>(), adam.getGrades());
+    }
+
+    @Test
+    public void testAddGrade() {
+        Student adam = new Student("Adam", 1);
+        adam.addGrade(90);
+        ArrayList<Integer> testGrades = new ArrayList<>();
+        testGrades.add(90);
+        assertEquals(adam.getGrades(), testGrades);
+    }
+
+    @Test
+    public void testGetGradeAverage() {
+        Student adam = new Student("Adam", 1);
+        adam.addGrade(90);
+        adam.addGrade(80);
+        assertEquals(adam.getGradeAverage(), 85, 0);
+    }
 }
